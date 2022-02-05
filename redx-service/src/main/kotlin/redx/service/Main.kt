4536787a62625fe33c58a4cce68433b9fb4029fb
John Hurst <john.b.hurst@gmail.com>
2022-02-06 09:14:34 +1300

    fix: Restored empty Main because we currently still require @SpringBootApplication to run tests.

---
+++ redx-service/src/main/kotlin/redx/service/Main.kt
@@ -0,0 +1,14 @@
+// Copyright 2022 JOhn Hurst
+// John Hurst
+// 2022-02-01
+
+package redx.service
+
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+open class Main  : CommandLineRunner {
+    override fun run(vararg args: String?) {
+    }
+}
